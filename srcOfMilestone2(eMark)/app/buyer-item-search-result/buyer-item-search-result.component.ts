e7febb75c4e730f46f8f62a7e407a2a64b057707
Steven-Guo-66 <63448158+Steven-Guo-66@users.noreply.github.com>
2020-05-02 23:15:40 +0800

    Add files via upload

---
+++ srcOfMilestone2(eMark)/app/buyer-item-search-result/buyer-item-search-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buyer-item-search-result',
+  templateUrl: './buyer-item-search-result.component.html',
+  styleUrls: ['./buyer-item-search-result.component.css']
+})
+export class BuyerItemSearchResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
